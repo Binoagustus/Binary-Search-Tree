be0a06718abe23de404bd1ca2b89ea1c276673e6
Binoagustus <binoagustus@gmail.com>
2022-11-19 00:00:37 +0530

    add 56 and then 30 and 70 to left and right nodes

--- BSTOperations.java
+++ BSTOperations.java
@@ -1,8 +1,22 @@
 package com.binary_tree;
 
-public class BSTOperations {
-	
-	public void add(int value) {
-		
+public class BSTOperations<K extends Comparable<K>>{
+	INode<K> root;
+
+	public void add(K value) {
+		if (root == null) {
+			root = new INode<K>(value);
+			System.out.println("root value is " + value);
+		}
+
+		if (value.compareTo(root.key) < 0) {
+			root.left = new INode<K>(value);
+			System.out.println("left " + value);
+			
+		} else if (value.compareTo(root.key) > 0) {
+			root.right = new INode<K>(value);
+			System.out.println("right " + value);
+		}
+
 	}
 }
