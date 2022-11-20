8bcc938844faab55345c95beed88c2e413842294
Binoagustus <binoagustus@gmail.com>
2022-11-20 20:33:28 +0530

    create binary tree with values

--- BSTOperations.java
+++ BSTOperations.java
@@ -1,22 +1,32 @@
 package com.binary_tree;
 
-public class BSTOperations<K extends Comparable<K>>{
+public class BSTOperations<K extends Comparable<K>> {
 	INode<K> root;
 
-	public void add(K value) {
-		if (root == null) {
-			root = new INode<K>(value);
-			System.out.println("root value is " + value);
-		}
+	public INode<K> add(K value) {
+		root = addRecursive(root, value);
+		System.out.println("root returned is " + root.key);
+		System.out.println("");
+		return root;
+	}
 
-		if (value.compareTo(root.key) < 0) {
-			root.left = new INode<K>(value);
-			System.out.println("left " + value);
-			
-		} else if (value.compareTo(root.key) > 0) {
-			root.right = new INode<K>(value);
-			System.out.println("right " + value);
+	public INode<K> addRecursive(INode<K> current, K key) {
+		if (current == null) {
+			current = new INode<K>(key);
+			System.out.println("Node " + key + " created ");
+			return current;
 		}
+		if (key.compareTo(current.key) < 0) {
+			current.left = addRecursive(current.left, key);
+			System.out.println(key + " is assigned left of " + current.key);
 
+		} else if (key.compareTo(current.key) > 0) {
+			current.right = addRecursive(current.right, key);
+			System.out.println(key + " is assigned right of " + current.key);
+
+		} else {
+			return current;
+		}
+		return current;
 	}
 }
